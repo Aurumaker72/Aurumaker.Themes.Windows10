b9f60c88788f5864c16bb6e2c7db8b33e951a256
Alex <48759429+Aurumaker72@users.noreply.github.com>
2023-08-07 17:10:59 +0200

    Add files

---
+++ Aurumaker.Themes.Windows10.Demo/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Aurumaker.Themes.Windows10.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Aurumaker.Themes.Windows10/Windows10Theme.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Avalonia.Markup.Xaml;
+using Avalonia.Styling;
+
+namespace Aurumaker.Themes.Windows10;
+
+public class Windows10Theme : Styles
+{
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Windows10Theme" /> class.
+    /// </summary>
+    /// <param name="sp">The parent's service provider.</param>
+    public Windows10Theme(IServiceProvider? sp = null)
+    {
+        AvaloniaXamlLoader.Load(sp, this);
+    }
+}
